7285340b9a2ec99167b0caca2f3d047244cd97b5
Andrew Cornejo <atcornejo3@gmail.com>
2022-11-04 15:36:37 -0700

    adjusted test to use database, adjusted app.js to require champions, previously configured setup.sql, ran setup-db, added model, ran first test and passed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,11 +1,14 @@
-// const pool = require('../lib/utils/pool');
-// const setup = require('../data/setup');
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-
 const { champions } = require('../lib/champions-data');
 
 describe('champions routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
   it('/champions should return a list of champions', async () => {
     const res = await request(app).get('/champions');
     const expected = champions.map((champions) => {
--- sql/setup.sql
+++ sql/setup.sql
@@ -6,22 +6,22 @@ CREATE TABLE champions (
     id BIGINT GENERATED ALWAYS AS IDENTITY, 
     name VARCHAR NOT NULL,
     title VARCHAR NOT NULL,
-    quote VARCHAR NOT NULL,
+    quote VARCHAR NOT NULL
 );
 
 INSERT INTO champions (name, title, quote)
 VALUES
-('Akshan', 'The Rogue Sentinel', 'Fixing the world, one scoundrel at a time'),
-('BelVeth', 'The Empress of the Void', 'I am the voice of the silence'),
-( 'Catilyn' ,'The Sheriff of Piltover' ,'Im on the case'),
+('Akshan','The Rogue Sentinel', 'Fixing the world, one scoundrel at a time'),
+('BelVeth','The Empress of the Void', 'I am the voice of the silence'),
+( 'Catilyn','The Sheriff of Piltover','Im on the case'),
 ( 'Cassiopea', 'The Serpents Embrace', 'There is no antidote for me'),
-( 'Garen' ,'Vanguard of Demacia' ,'For Demacia!'),
+( 'Garen','Vanguard of Demacia' ,'For Demacia!'),
 ( 'Leona', 'Aspect of the Solari', 'The Sun will never set'),
-( 'Lee Sin' ,'Monk of Ionia','Your will. My hands'),
+( 'Lee Sin','Monk of Ionia','Your will. My hands'),
 ( 'Poppy', 'Hero of Legend', 'Im no hero, just a yordle with a hammer'),
-( 'Shen' ,'Eye of Twilight' ,'Youre already dead, you just havent caught up yet'),
-( 'Skarner', 'The Last Brackern' ,'Feel my sting!'),
-( 'Taric' ,'Shield of Valoran' ,'Whatchu know bout gems'),
-( 'Viego', 'King of Camavor', 'Where is she?!'),
+( 'Shen' ,'Eye of Twilight','Youre already dead, you just havent caught up yet'),
+( 'Skarner', 'The Last Brackern','Feel my sting!'),
+( 'Taric' ,'Shield of Valoran','Whatchu know bout gems'),
+( 'Viego','King of Camavor','Where is she?!');
 
 
